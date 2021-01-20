2e7ecea3843ffd7fac58aad181d324f5724c4fd3
sanchanchourasia <sanchanchourasia.ips@gmail.com>
2021-01-20 23:10:50 +0530

    initial commit at Completed

---
+++ src/components/my-section/section.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './mycarousal-style.css';
+
+
+const MyCarousal = () => {
+    return (
+        <div className="home">
+            
+        </div>
+    )
+}
+
+export default MyCarousal
