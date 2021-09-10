2a69800294c2dfe72ae319bd3371e19b434e339a
Sanchan Chourasia <76470119+snchn@users.noreply.github.com>
2021-09-10 12:20:19 +0530

    Update titletypewriter-component.jsx

--- src/components/title-typewriter/titletypewriter-component.jsx
+++ src/components/title-typewriter/titletypewriter-component.jsx
@@ -41,7 +41,7 @@ const TitleMessage = () => (
           <div className="sub">
             <Typewriter
               options={{
-                strings: ["Web Developer", "Coach", "Learner"],
+                strings: ["Web Developer", "Enthusiastic ", "Learner"],
                 autoStart: true,
                 loop: true,
                 delay: 50
